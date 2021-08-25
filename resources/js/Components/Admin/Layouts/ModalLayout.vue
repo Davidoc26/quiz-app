d66d0cac82251035bcec90c4bb7e53efaa6e190d
David Eritsyan <dav.eritsyan@gmail.com>
2021-08-25 15:37:00 +0400

    modal's refactoring

---
+++ resources/js/Components/Admin/Layouts/ModalLayout.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="flex flex-col h-full">
+        <div class="flex flex-col bg-gray-200 justify-between h-full">
+            <div class="flex justify-center">
+                <p class="font-semibold text-md mt-4">
+                    <slot name="title"></slot>
+                </p>
+            </div>
+            <div class="flex justify-center">
+                <slot name="inputs"></slot>
+            </div>
+            <div class="flex flex-shrink">
+                <button @click="$emit('close')" class="p-4 bg-red-400 rounded-sm w-1/2">Close</button>
+                <slot name="actionButton"></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ModalLayout"
+}
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/Components/Admin/NewQuestionModal.vue
+++ resources/js/Components/Admin/NewQuestionModal.vue
@@ -1,28 +1,26 @@
 <template>
-    <div class="flex flex-col h-full">
-        <div class="flex flex-col bg-gray-200 justify-between h-full">
-            <div class="flex justify-center">
-                <p class="font-semibold text-md mt-4">Add new question</p>
-            </div>
-            <div class="flex justify-center">
-                <input
-                    class="w-full px-4 py-2 mt-2 text-base text-black transition duration-500 ease-in-out transform border-transparent rounded-lg bg-blueGray-100 focus:border-blueGray-500 focus:bg-white focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-    2"
-                    type="text" name="title" v-model="question.question" required>
-            </div>
-            <div class="flex flex-shrink">
-                <button @click="$emit('close')" class="p-4 bg-red-400 rounded-sm w-1/2">Close</button>
-                <button @click="createQuestion" class="p-4 bg-blue-400 rounded-sm w-1/2">Create</button>
-            </div>
-        </div>
-    </div>
-
+    <ModalLayout>
+        <template #title>
+            <p class="font-semibold text-md mt-4">Add new question</p>
+        </template>
+        <template #inputs>
+            <input
+                class="w-full px-4 py-2 mt-2 text-base text-black transition duration-500 ease-in-out transform border-transparent rounded-lg bg-blueGray-100 focus:border-blueGray-500 focus:bg-white focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-    2"
+                type="text" name="title" v-model="question.question" required>
+        </template>
+        <template #actionButton>
+            <button @click="createQuestion" class="p-4 bg-blue-400 rounded-sm w-1/2">Create</button>
+        </template>
+    </ModalLayout>
 </template>
 
 <script>
 import {eventBus} from "@/app";
+import ModalLayout from "./Layouts/ModalLayout";
 
 export default {
     name: "NewQuestionModal",
+    components: {ModalLayout},
     data() {
         return {
             question: {
